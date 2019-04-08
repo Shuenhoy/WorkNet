ec5f3f725c941ffcd73c5a1efd60744890e9efe2
Haoran SUN <3170105365@zju.edu.cn>
2019-04-08 18:39:24 +0800

    add coafile

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+"""main module"""
 import aiohttp
 import asyncio
 
-async def fetch(session, url):
+
+async def fetch(session: aiohttp.ClientSession, url: str):
     async with session.get(url) as response:
         return await response.text()
+
 
 async def main():
     async with aiohttp.ClientSession() as session:
